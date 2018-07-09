f466a24d627dcf901f69fc84e357cb3b52c32fd5
Max Caminer <max.caminer@student.uts.edu.au>
2018-07-10 00:59:51 +1000

    Python file now creates a map with all the capital cities as markers

--- WebMap.py
+++ WebMap.py
@@ -1,1 +1,23 @@
-print("Hello World")
+import folium
+import pandas
+data = pandas.read_csv("worldcapitals.csv", encoding = "latin1")
+d1 = data.loc[:, "Name":"Longitude"]
+
+
+map = folium.Map(location=[0, 0], zoom_start=2, tiles ="Mapbox Bright")
+
+city_name = list(data.ix[:, 1])
+country = list(data.ix[:, 2])
+lat = list(data["Latitude"])
+long = list(data["Longitude"])
+
+
+
+fg = folium.FeatureGroup(name= "World Capitals")
+
+for lt, lg, city, cy in zip(lat, long, city_name, country):
+     fg.add_child(folium.CircleMarker(location=[lt, lg], radius=2, fill=True, popup=folium.Popup(str(city) + ', ' + str(cy), parse_html=True), icon=folium.Icon(color='black')))
+
+map.add_child(fg)
+
+map.save("Map1.html")
